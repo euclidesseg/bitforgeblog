8b381c1264593deaae432243ecefe6956acad5db
euclidesseg <euclides2696@gmail.com>
2023-12-20 15:42:59 -0500

    angular update

--- src/app/bitforgeblog/pages/layout-page/layout-page.component.ts
+++ src/app/bitforgeblog/pages/layout-page/layout-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styleUrls: ['./layout-page.component.css']
+  styleUrls: ['./layout-page.component.css', './layout-page.component-movile.css']
 })
 export class LayoutPageComponent {
 
