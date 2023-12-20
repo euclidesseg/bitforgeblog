f27b34185f02330924b68bd10679d711c5085cc2
euclidesseg <euclides2696@gmail.com>
2023-12-20 17:02:43 -0500

    created the messagespage

--- src/app/bitforgeblog/bitforgeblog.module.ts
+++ src/app/bitforgeblog/bitforgeblog.module.ts
@@ -3,18 +3,20 @@ import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
 import { BlogsPageComponent } from "./pages/blogs-page/blogs-page.component";
 import { BitForgeRoutingModule } from "./bitforgeblog-routing.module";
 import { CommonModule } from "@angular/common";
+import { MessagesComponent } from "./components/messages/messages.component";
+import { MessageComponent } from "./pages/message/message.component";
 
 @NgModule({
     declarations:[
       LayoutPageComponent,
-      BlogsPageComponent
+      BlogsPageComponent,
+      MessageComponent
     ],
     imports:[
       BitForgeRoutingModule,
       CommonModule
     ],
     exports:[
-
     ]
 })
 export class BitForgeModule{}
