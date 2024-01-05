2417b5b7533d93d82eb63886889d5df8b3219a87
euclidesseg <euclides2696@gmail.com>
2024-01-05 17:28:25 -0500

    uptade angular sintax

--- src/app/bitforgeblog/pages/layout-page/layout-page.component.ts
+++ src/app/bitforgeblog/pages/layout-page/layout-page.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styleUrls: ['./layout-page.component.css', './layout-page.component-movile.css']
+  styleUrls: [
+    './layout-page.component.css',
+    './layout-page.component-movile.css',
+  ],
 })
 export class LayoutPageComponent {
-  
+  public routesItems = [
+    { label: '', class: 'bi bi-house', route: './blogs' },
+    { label: '', class: '', route: '' },
+    { label: '', class: '', route: '' },
+    { label: '', class: '', route: '' },
+  ];
 }
